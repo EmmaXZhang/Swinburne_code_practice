01fea43d2ea5b9d1da6c16d2d395e518ad9b26f4
EmmaZhang <emmazxd@gmail.com>
2024-08-05 11:20:16 +1000

    add questions

--- hello.py
+++ hello.py
@@ -1,4 +1,26 @@
-print("Enter your name")
-x= input()
-print("Hello " + x )
+# print("Enter your name")
+# x= input()
+# print("Hello " + x )
 
+# print('hello world')
+# print("second hello")
+# print("third hello")
+
+# Q1:
+# num1 = int(input('Enter first integer: '))
+# num2 = int(input('Enter second integer: '))
+# print(num1+num2)
+
+# Q2:
+s1 = "hello"
+s2 = "world"
+print(s1+s2)
+#
+# # Q3:
+# currentYear = 2024
+# age = int(input("Enter age: "))
+# print(f"Born: {currentYear - age}")
+#
+# # Q4:
+# oddNum = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+# print(sum(oddNum))
