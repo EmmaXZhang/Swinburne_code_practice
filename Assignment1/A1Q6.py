6f5b13e250b00f4e83cb402a93cb57ffb8141144
EmmaZhang <emmazxd@gmail.com>
2024-09-15 16:20:40 +1000

    finish all

--- Assignment1/A1Q6.py
+++ Assignment1/A1Q6.py
@@ -1,6 +1,4 @@
 
-user_choice = input("Choose menu option: \n 1.Load records \n 2.Display \n 3.Add record \n 4.Delete record \n 5.Save records \n 6.Exit \n Select: ")
-
 # Load_records function
 import csv
 # global array
@@ -41,7 +39,9 @@ def add_data():
     price = float(input("Price: "))
     stock = int(input("Quantity in Stock: "))
     supplier = input("Supplier: ")
-    product_ids.append(product_ids[-1] + 1)
+    # create new id variable
+    new_id = product_ids[-1] + 1 if product_ids else 1001
+    product_ids.append(new_id)
     product_names.append(product_name)
     categories.append(category)
     prices.append(price)
@@ -49,26 +49,69 @@ def add_data():
     suppliers.append(supplier)
 
 # Delete record
+def delete_data():
+    # no products in the file
+    if not product_ids:
+        print("No records to delete.")
+        return
+    # else display data
+    display_data()
 
+    # check if id exist,
+    # In a while True loop, the loop itself will run indefinitely until you explicitly break out of it.
+    while True:
+        try:
+            delete_id = int(input("Enter the Product Id to be deleted: "))
+            if delete_id in product_ids:
+                break
+            else:
+                print("Product ID not found. Please try again.")
+        except ValueError:
+            print("Invalid input. Please enter a valid numeric Product ID.")
+    # find ID, and remote it
+    delete_index = product_ids.index(delete_id)
+    product_ids.pop(delete_index)
+    product_names.pop(delete_index)
+    categories.pop(delete_index)
+    prices.pop(delete_index)
+    quantity_in_stock.pop(delete_index)
+    suppliers.pop(delete_index)
+    print(f"Product with ID {delete_id} deleted successfully.")
 
 # Save record
-
+def save_data():
+    with open(csv_file, mode="w", newline="") as product_inventory:
+        writer = csv.writer(product_inventory)
+        writer.writerow(["Product_Id", "Product_Name", "Category", "Price", "Quantity_in_Stock", "Supplier"])
+        for i in range(len(product_ids)):
+            writer.writerow([product_ids[i], product_names[i], categories[i], prices[i], quantity_in_stock[i], suppliers[i]])
+        print("Records saved successfully.")
 
 # Exit
+def exit_app():
+    print("Exiting the application successfully.")
+    exit()
 
 
+def menu():
+    while True:
+        print("Choose menu option: \n 1.Load records \n 2.Display \n 3.Add record \n 4.Delete record \n 5.Save records \n 6.Exit")
+        user_choice = input("Select: ")
 
+        match user_choice:
+            case 1:
+                load_record(csv_file)
 
+            case 2:
+                display_data()
+            case 3:
+                add_data()
+            case 4:
+                delete_data()
+            case 5:
+                save_data()
+            case 6:
+                exit_app()
+            case _:
+                print("Invalid option. Please select from 1 to 6.")
 
-
-
-def switch_case(value):
-    match value:
-        case 1:
-            return "One"
-        case 2:
-            return "Two"
-        case 3:
-            return "Three"
-        case _:
-            return "Unknown"
